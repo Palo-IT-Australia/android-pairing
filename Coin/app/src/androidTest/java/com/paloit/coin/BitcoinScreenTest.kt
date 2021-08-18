3f7e6143878c137baee243c6f7399448ea3c0651
Andrei Maslennikov <cheze92@gmail.com>
2021-08-18 18:05:36 +1000

    implement instrumented android tests (#6)

---
+++ Coin/app/src/androidTest/java/com/paloit/coin/BitcoinScreenTest.kt
@@ -0,0 +1,27 @@
+package com.paloit.coin
+
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.paloit.coin.app.BitcoinScreen
+import com.paloit.coin.app.MainActivityUiState
+import org.junit.Rule
+import org.junit.Test
+
+class BitcoinScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun given_bitcoin_price_is_known_when_app_retrieves_bitcoin_price_then_show_bitcoin_price() {
+        val mainActivityUiState = MainActivityUiState(false, "12.59")
+        composeTestRule.setContent {
+            BitcoinScreen(uiState = mainActivityUiState) {}
+        }
+
+        composeTestRule.onNode(
+            hasText("12.59"),
+            useUnmergedTree = true
+        ).assertExists()
+    }
+}
