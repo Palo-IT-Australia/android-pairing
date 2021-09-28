e280f340f9a457972a85aed67235aa3db34f923c
Andrei Maslennikov <amaslennikov@palo-it.com>
2021-09-28 16:57:09 +1000

    add UI test for currencies screen

---
+++ Coin/app/src/androidTest/java/com/paloit/coin/CurrenciesScreenTest.kt
@@ -0,0 +1,34 @@
+package com.paloit.coin
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.hasAnyChild
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.paloit.coin.app.supportedCurrencies.CurrenciesUiState
+import com.paloit.coin.app.supportedCurrencies.ListScreen
+import com.paloit.coin.platform.repository.data.Currency
+import org.junit.Rule
+import org.junit.Test
+
+class CurrenciesScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @ExperimentalMaterialApi
+    @Test
+    fun given_when_then() {
+        val uiState = CurrenciesUiState(
+            false, listOf(
+                Currency("Australia", "AUD")
+            )
+        )
+
+        composeTestRule.setContent {
+            ListScreen(uiState = uiState) { }
+        }
+
+        composeTestRule.onAllNodes(hasAnyChild(hasText("Australia")), useUnmergedTree = true)
+            .assertCountEquals(1)
+    }
+}
